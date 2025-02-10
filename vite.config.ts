ad8a1e029cd0d73d7680e39d42adf93a297b71d4
Caiohvboas <116687690+Caiohvboas@users.noreply.github.com>
2025-02-10 14:19:56 -0300

    Atualização do arquivo Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  
-  base: '/my-portfolio', // Caminho base relativo ao seu repositório no GitHub Pages
-  // outras configurações
+  base: '/my-portfolio/', // Caminho base relativo ao seu repositório no Github Pages
+  server: {
+    mimeTypes: {
+      'js': 'application/javascript',
+      'mjs': 'application/javascript'
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 });
